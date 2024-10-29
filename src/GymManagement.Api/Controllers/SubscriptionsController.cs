f2b0dbd37885475a68a29fa73b5b609fe477fe9c
huydq2152 <quanghuy2152@gmail.com>
2024-10-29 23:46:13 +0700

    add code for application layer and infrastructure layer

---
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,27 @@
+﻿using GymManagement.Application.Services;
+using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    private readonly ISubscriptionService _subscriptionService;
+
+    public SubscriptionsController(ISubscriptionService subscriptionService)
+    {
+        _subscriptionService = subscriptionService;
+    }
+
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+    {
+        var subscriptionId =
+            _subscriptionService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
+        var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+        
+        return Ok(response);
+    }
+}
---
+++ src/GymManagement.Application/Services/SubscriptionService.cs
@@ -0,0 +1,9 @@
+﻿namespace GymManagement.Application.Services;
+
+public class SubscriptionService: ISubscriptionService
+{
+    public Guid CreateSubscription(string subscriptionType, Guid adminId)
+    {
+        return Guid.NewGuid();
+    }
+}
